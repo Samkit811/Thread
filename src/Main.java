b35c2c0cd2378a11edf18aa9589f75e2130f66ec
samkits <samkitsalot0@gmail.com>
2025-04-22 13:15:29 +0530

    Added thread invoking logic in main class.

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,15 @@ public class Main {
         ProduceTask produceTask = new ProduceTask(sharedResource);
         ConsumeTask consumeTask = new ConsumeTask(sharedResource);
 
+        //Thread making
+        Thread producerThread = new Thread(produceTask);
+        Thread consumerThread = new Thread(consumeTask);
+
+        // Thread is in runnable state
+        producerThread.start();
+        consumerThread.start();
+
+        System.out.println("Main method ends");
+
     }
 }
