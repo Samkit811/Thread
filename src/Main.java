2bdaf5ec9d7c38fc7d99639c9f325f5d1a996511
samkits <samkitsalot0@gmail.com>
2025-04-22 14:46:09 +0530

    Added shared Buffer class logic implementation in main class

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
         System.out.println("Main method start" + " at time: " + System.currentTimeMillis());
 
+        System.out.println("------------- Shared Resource Implementation --------------");
         SharedResource sharedResource = new SharedResource();
 
         ProduceTask produceTask = new ProduceTask(sharedResource);
@@ -15,6 +16,41 @@ public class Main {
         // Thread is in runnable state
         producerThread.start();
         consumerThread.start();
+
+        System.out.println("------------- Shared Buffer Implementation --------------");
+
+        // Shared Buffer Implementation start
+        SharedBuffer sharedBuffer = new SharedBuffer(2);
+
+        Thread producerBufferThread1 = new Thread(() -> {
+            sharedBuffer.addBufferItem();
+        });
+
+        Thread consumerBufferThread1 = new Thread(() -> {
+            sharedBuffer.consumeBufferItem();
+        });
+
+        Thread producerBufferThread2 = new Thread(() -> {
+            sharedBuffer.addBufferItem();
+        });
+
+        Thread consumerBufferThread2 = new Thread(() -> {
+            sharedBuffer.consumeBufferItem();
+        });
+
+        Thread producerBufferThread3 = new Thread(() -> {
+            sharedBuffer.addBufferItem();
+        });
+
+        Thread consumerBufferThread3 = new Thread(() -> {
+            sharedBuffer.consumeBufferItem();
+        });
+
+        consumerBufferThread1.start();
+        producerBufferThread1.start();
+        producerBufferThread2.start();
+        producerBufferThread3.start();
+        consumerBufferThread2.start();
 
         System.out.println("Main method ends" + " at time: " + System.currentTimeMillis());
 
